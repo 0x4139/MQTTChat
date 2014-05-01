2a53f05fca7bcb284a5fe7832ec4f0ca19403513
Vali Malinoiu <0x4139@gmail.com>
2014-05-01 17:06:48 +0300

    Added mqtt with public test server

--- chat.js
+++ chat.js
@@ -1,7 +1,9 @@
 var Hapi = require('hapi');
 var jwt = require('jsonwebtoken');
 var server = Hapi.createServer('0.0.0.0', 5445);
+var mqtt = require('mqtt');
 
+var client = mqtt.createClient(1883, 'test.mosquitto.org');
 var privateKey = 'VODKAPUTINBALALAIKA';
 
 var invalidTokenMSG={
@@ -16,6 +18,7 @@ server.route({
     method: 'POST',
     path: '/connect',
     handler: function (request, reply) {
+      client.publish('room/'+request.payload.room+'/join', request.payload.username);
       reply(jwt.sign(request.payload,privateKey));
     }
 });
@@ -25,8 +28,11 @@ server.route({
     path: '/typing',
     handler: function (request, reply) {
       jwt.verify(request.payload.token, privateKey, function(err, decoded) {
-        if(err)
-          reply(invalidTokenMSG);
+        if(err){
+            reply(invalidTokenMSG);
+            return;
+          }
+        client.publish('room/'+decoded.room+'/typing', decoded.username);
         reply(requestOK);
       });
    }
@@ -37,8 +43,12 @@ server.route({
     path: '/message',
     handler: function (request, reply) {
       jwt.verify(request.payload.token, privateKey, function(err, decoded) {
-        if(err)
+        if(err){
           reply(invalidTokenMSG);
+          return;
+        }
+
+        client.publish('room/'+decoded.room+'/message', decoded.username+':'+decoded.message);
         reply(requestOK);
       });
    }
