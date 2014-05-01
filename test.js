b8da050eb6281c611e72444f0f730ca965e10bba
Vali Malinoiu <0x4139@gmail.com>
2014-05-01 15:17:31 +0300

    #1 adds CI with travis and basic test file

---
+++ test.js
@@ -0,0 +1,12 @@
+var assert = require("assert")
+
+describe('ChatJS',function(){
+
+  it('should succeed',function(){
+    assert.equal(0,0);
+  });
+
+  it('should crash',function(){
+    assert.equal(1,0);
+  });
+})
