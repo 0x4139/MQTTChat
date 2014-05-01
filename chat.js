35d3b91b940048ac04c09f54b1e24b2007218e2c
Vali Malinoiu <0x4139@gmail.com>
2014-05-01 15:04:35 +0300

    #1 Adds hapijs,jwt, and /connect route

---
+++ chat.js
@@ -0,0 +1,30 @@
+var Hapi = require('hapi');
+var jwt = require('jsonwebtoken');
+var server = Hapi.createServer('0.0.0.0', 5445);
+
+var validate = function (decodedToken, callback) {
+
+    console.log(decodedToken);
+
+    if (decodedToken) {
+      console.log(decodedToken.accountId.toString());
+    }
+    return callback(null, true, decodedToken)
+};
+
+
+server.pack.require('hapi-auth-jwt', function (err) {
+
+    var privateKey = 'fiphejflhaskjfajhqwouehoqwuhewqojheqwkjebqwkh';
+    server.auth.strategy('token', 'jwt', { key: privateKey,  validateFunc: validate });
+
+    server.route({
+        method: 'POST',
+        path: '/connect',
+        handler: function (request, reply) { reply(jwt.sign(request.payload,privateKey));}
+    });
+});
+
+
+server.start();
+console.log('MQTTChat started!')
